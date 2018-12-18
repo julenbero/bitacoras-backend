af70309aa2d6e1993ca2968fd92a5967bfb03374
Javier Rojas <javierrojas6@gmail.com>
2018-12-17 20:12:06 -0500

    passport admin

--- modules/authentication-verifier.js
+++ modules/authentication-verifier.js
@@ -2,6 +2,7 @@
 
 //se define passport global para usarlo en todos los routes
 passport = require('passport');
+passportAdmin = require('passport');
 var Strategy = require('passport-http-bearer').Strategy;
 
 passport.use(new Strategy((token, done) => {
@@ -9,6 +10,15 @@ passport.use(new Strategy((token, done) => {
         .findOne({ where: { token } })
         .then(session => {
             done(null, !!session);
+        });
+}
+));
+
+passportAdmin.use(new Strategy((token, done) => {
+    models.User
+        .findOne({ where: { token } })
+        .then(user => {
+            done(null, user && user.Role && user.Role.nombre == 'administrador');
         });
 }
 ));
